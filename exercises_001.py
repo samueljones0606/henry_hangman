1f6da97903158dead5ae81d531f1a6726a213e5b
Samuel Jones <samueljones0606@gmail.com>
2020-04-04 11:42:19 +0100

    exercises 1

---
+++ exercises_001.py
@@ -0,0 +1,21 @@
+def main()
+
+    word = "description"
+
+    # write code here to get the length of the word, and put it in a
+    # variable called "length"
+
+
+
+    # write code here to take 1 letter as input and put it in a variable called "guess"
+
+
+
+    # write code here to print a "-" for every letter in the word, unless the letter
+    # is equal to the guess, in whcih case print the letter.
+    # for example, if the word is "fart" and the guess is "a", print "-a--".
+    # if the guess is "x" which is not in the word, your code should print "----"
+
+
+if __name__ == "__main__"
+    main()

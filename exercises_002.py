40e2be8fa140287ac2d5045775e6b6320699d0ed
Z6Wv <hrjzxy@gmail.com>
2020-04-21 19:19:06 +0000

    i made this gud and ur bad at making it good cuz im bettr

--- exercises_002.py
+++ exercises_002.py
@@ -2,7 +2,8 @@
 
 Exercises with variables (revision - we've done most of this before!
 
-This script contains 3 exercises, ecah one is contiained in a separate function. If you run the script, all three functions will be called (you can see this at the bottom of the script)
+This script contains 3 exercises, each
+one is contiained in a separate function. If you run the script, all three functions will be called (you can see this at the bottom of the script)
 However at the moment they dont' print anythign at all, so it will look like nothig has happened. Your job is to go through each of the three functions and do what it says in the
 instruction comments, so that each will print an answer. You can run the scriopt as many times as you like to check if your code is working.
 
@@ -11,33 +12,40 @@ when you have finishes, you might like to try adding a fourth function, by writi
 Good luck!
 
 """
-
+import random
 
 def exercise_200():
 
     seconds_per_minute = 60
     minutes_per_hour = 60
 
-    # create a variables called "hours_per_day" and "days_per_year" and assign the the right values
+    # create variables called "hours_per_day" and "days_per_year" and assign them the right values
+
+    hours_per_day = 24
+    days_per_year = 365
 
     # at the moment, this line calcuates the number of seconds in an hour. using your new variables,
     # change it to calculate the number of seconds in a year
     
-    answer = seconds_per_minute * minutes_per_hour
+    answer = seconds_per_minute * minutes_per_hour * hours_per_day * days_per_year
 
     # write a line of code to print the results, using the "print" function
+    print(answer)
 
     
 def exercise_201():
 
-    # Mummy gives each of Ed, Lucy and Henry £3.
+    # Mummy gives each of Ed, Lucy and Henry £2.
     # Daddy gives each of Ed, Lucy and Henry $4.
     # Which of these two answers do you think is the total amoutn of pocket money?
 
     answer_1 = 2 + 4 * 3
     answer_2 = (2 + 4) * 3
 
+
     # Write a line of code to print the correct answer for the total amount of pocket money
+    
+    print(answer_2)
 
 def exercise_202():
 
@@ -47,21 +55,25 @@ def exercise_202():
 
     toilet = bum
     fart = toilet
+    print(toilet)
+    print(bum)
 
     # what is the the value of "toilet"?
     # what is the value of "bum"
     # print the  value of "fart" added to the value of "toilet"
+    lil_toilet_seat = fart + toilet
+    print(lil_toilet_seat)
 
 def exercise_203():
 
     # Grandads magic money machine prints 10 gold coins every day
-    new_coins = 10
+    new_coins = 12
 
     # he runs it every day for a year
     days_in_a_year = 365
 
     # but every sunday, he gives 5 gold coins to Daddy
-    daddys_coins = 5
+    daddys_coins = 10
 
     # there are 52 Sundays in a year
     weeks_in_a_year = 52
@@ -70,14 +82,20 @@ def exercise_203():
     henrys_coins = new_coins * days_in_a_year - daddys_coins * weeks_in_a_year
 
     # Un-comment this line to find out how many coins Henry gets at the end of the year, and run the script to find out
-    # print("Henry had {} coins".format(henrys_coins))
+    print("Henry had {} coins".format(henrys_coins))
 
     # Now change the machine so it makes 12 coins a day, and Grandad gives 10 coins every Sunday to Daddy.
     # How many coins does Henry get now? is Henry richer or poorer than before?
+
+def exercise_204():
+    n = random.randint(0, 2)
+    foodz = ["Poop", "other poop", "more poop"]
+    my_food = foodz[n]
+    print("i eat {}".format(my_food))
     
 
-if __name__ == "__main__":
-    
-    exercise_201()
-    exercise_202()
-    exercise_203()
+exercise_200()
+exercise_201()
+exercise_202()
+exercise_203()
+exercise_204()
